221f7c4f5f8e3a332c9c50d54115d2a9b108174a
zohaib10 <zohaib.m.ahmad@gmail.com>
2018-01-20 18:30:21 -0800

    Upload picture is working better and changed styling for index1.html

--- scripts.js
+++ scripts.js
@@ -5,6 +5,26 @@ function upload() {
 function uploadFile() {
   //console.log("in this function");
   // where we find the file handle
-  var selectedFile = document.getElementById('myFile').files[0];
-  console.log(selectedFile.name);
+    var selectedFile = document.getElementById('myFile').files[0];
+    console.log(selectedFile);
+    var div = document.createElement("div");
+    div.setAttribute("class","d221");
+    var fC = document.createElement("div");
+    fC.setAttribute("class","j1");
+    var sC = document.createElement("div");
+    sC.setAttribute("class","j2");
+    
+    fC.style.backgroundImage = "url("+selectedFile.name+")";
+    var para = document.createElement("p");
+    para.innerHTML = "Calories: 2200";
+    sC.appendChild(para);
+    div.appendChild(fC);
+    div.appendChild(sC);
+    var parent = document.getElementById("toAppend");
+    console.log(parent);
+    console.log(div);
+    parent.appendChild(div);
+    
+    
+
 }
